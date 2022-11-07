071737d1cce66c6b365168271e729849c33d67bf
takuma-ru <kcattdev@gmail.com>
2022-11-07 11:03:35 +0900

    [Add] google font (Noto Sans JP)

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -3,6 +3,14 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  head: {
+    link: [
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap' },
+    ]
+  },
+
   modules: ['@nuxt/content'],
 
   vite: {
