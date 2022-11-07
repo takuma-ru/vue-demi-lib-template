ceb66506a0a56daab696607ce47380b86fd2ccaa
takuma-ru <kcattdev@gmail.com>
2022-11-07 11:10:16 +0900

    [Add] loading progress bar

--- docs/app.vue
+++ docs/app.vue
@@ -2,17 +2,32 @@
   <div>
     <Header />
     <NuxtPage />
+    <NuxtLoading ref="loading" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import 'material-icons/iconfont/material-icons.css'
+import NuxtLoading from './components/layouts/NuxtLoading.vue';
 
+const loading = ref(null)
+
+const nuxtApp = useNuxtApp()
+
+nuxtApp.hook('page:start', () => {
+  loading.value.start()
+})
+
+nuxtApp.hook('page:finish', () => {
+  loading.value.finish()
+  window.scrollTo(0, 0)
+})
 </script>
 
 <style lang="scss">
 html, body {
   margin: 0px;
+  font-family: 'Noto Sans JP', sans-serif;
 }
 
 main {
