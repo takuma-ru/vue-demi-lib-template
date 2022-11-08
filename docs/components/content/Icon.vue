4fdef2574b6c824682ebbd581abaeac7511efb3b
takuma-ru <kcattdev@gmail.com>
2022-11-08 10:06:05 +0900

    [Add: docs] Icon Component

---
+++ docs/components/content/Icon.vue
@@ -0,0 +1,42 @@
+<template>
+  <span :class="`material-icons-${props.fill ? 'round' : 'outlined'}`">
+    {{ props.icon }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+import { IconNameType } from '~~/types/icon/iconName';
+
+/* -- type, interface -- */
+export interface IProps {
+  icon: IconNameType
+  size?: string
+  color?: string
+  fill?: boolean
+  wght?: number
+}
+
+/* -- props, emit -- */
+const props = withDefaults(defineProps<IProps>(), {
+  icon: 'face',
+  size: '24px',
+  color: null,
+  fill: false,
+  wght: 500
+})
+
+/* -- store -- */
+/* -- variable(ref, reactive, computed) -- */
+/* -- function -- */
+/* -- watch -- */
+/* -- life cycle -- */
+</script>
+
+<style lang="scss" scoped>
+.material-icons-outlined, .material-icons-round {
+  font-size: v-bind("props.size");
+  font-weight: v-bind('props.wght');
+  color: v-bind('props.color');
+  user-select: none;
+}
+</style>
