59b7f7f9eaa33f29b539f30472a8f37e2588ad96
takuma-ru <kcattdev@gmail.com>
2022-11-10 22:14:05 +0900

    [Add: docs] Index page

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -1,15 +1,78 @@
 <template>
-  <main>
-    <div>
-      index
+  <main id="index">
+    <h1 class="title lib">
+      vue-swipe-modal
+    </h1>
+    <h1 class="title feature">
+      Modal window that can be swiped to close
+    </h1>
+    <h1 class="title name">
+      takuma-ru
+    </h1>
+    <div class="buttons">
+      <CButton
+        to="/get-started/vue2"
+      >
+        Get Started
+      </CButton>
+      <CButton
+        outlined
+        to="https://github.com/takuma-ru"
+      >
+        View on GitHub
+      </CButton>
     </div>
   </main>
 </template>
 
 <script lang="ts" setup>
+import { useColorStore } from '~/store/colorStore';
 
+const {
+  color
+} = useColorStore()
 </script>
 
 <style lang="scss" scoped>
+#index {
+  display: flex;
+  flex-flow: column;
 
+  .title {
+    position: relative;
+
+    margin: 36px 0px;
+
+    &::after {
+      position: absolute;
+      top: -36px;
+      left: 0px;
+
+      color: v-bind("color.theme.subText");
+      line-height: 36px;
+      font-size: 16px;
+      font-weight: 400;
+    }
+  }
+
+  .lib::after {
+    content: 'This is';
+  }
+
+  .feature::after {
+    content: 'Feature is';
+  }
+
+  .name::after {
+    content: 'Made by';
+  }
+
+  .buttons {
+    display: flex;
+
+    #Button {
+      margin-right: 1.5rem;
+    }
+  }
+}
 </style>
