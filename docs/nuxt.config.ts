eb1274a643948c8360612bb08b1c13cc7d77f096
takuma-ru <kcattdev@gmail.com>
2022-11-10 12:06:53 +0900

    [Update: docs] "nuxt": '3.0.0-rc.12' => `3.0.0-rc.13`

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,16 +1,17 @@
-import { defineNuxtConfig } from 'nuxt'
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
 
-  head: {
-    link: [
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap' },
-    ]
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: "" },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap' },
+      ]
+    },
   },
 
   modules: ['@nuxt/content', '@nuxtjs/color-mode'],
