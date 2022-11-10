dbd762cd5ffb02812b5382813037a1d302bf4490
takuma-ru <kcattdev@gmail.com>
2022-11-10 18:00:32 +0900

    [Update: docs] Header

--- docs/components/Header.vue
+++ docs/components/Header.vue
@@ -1,21 +1,74 @@
 <template>
   <header id="app-header">
-    header
+    <div class="title">
+      <img
+        src="https://nuxtjs.org/design-kit/colored-logo.svg"
+        alt="logo"
+      >
+      <div class="text">
+        lib-name
+      </div>
+    </div>
+    <div class="buttons">
+      <Button
+        :icon="colorMode === 'dark' ? 'dark_mode': 'light_mode'"
+        is-icon
+        @click="switchMode()"
+      />
+    </div>
   </header>
 </template>
 
 <script lang="ts" setup>
+import Button from '~/components/content/CButton.vue'
+import { useColorModeStore } from '~/store/colorModeStore';
 
+/* -- type, interface -- */
+
+/* -- props, emit -- */
+
+/* -- store -- */
+const {
+  colorMode,
+  switchMode
+} = useColorModeStore()
+
+/* -- variable(ref, reactive, computed) -- */
+/* -- function -- */
+/* -- watch -- */
+/* -- life cycle -- */
 </script>
 
 <style lang="scss" scoped>
 #app-header {
   display: flex;
   align-items: center;
+  justify-content: space-between;
 
-  width: min(100vw, 960px);
+  width: min(calc(100vw - 64px), 800px);
   height: 64px;
   padding: 0px 32px;
   margin: auto;
+
+  .title {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 32px;
+      height: 32px;
+      margin-right: 1em;
+    }
+
+    .text {
+      font-size: 1.5em;
+      font-weight: 500;
+    }
+  }
+
+  .buttons {
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
