9460cfd359a38f333c34fdb1fc9f996639cb1eb4
takuma-ru <kcattdev@gmail.com>
2022-12-15 21:10:45 +0900

    [Update: docs] eslint

--- docs/.eslintrc.js
+++ docs/.eslintrc.js
@@ -14,7 +14,10 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: [
+    "vue",
+    "@typescript-eslint"
+  ],
   rules: {
     'vue/multi-word-component-names': 'off'
   },
